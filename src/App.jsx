b9a55aa1a74fa7e4ec56202089f0db6305906576
Dempsey845 <dempseynewton1@gmail.com>
2025-06-12 13:34:44 +0100

    Firebase setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,13 @@
+import { useEffect } from "react";
+
 function App() {
+  useEffect(() => {
+    fetch("https://helloworld-6x5volitjq-uc.a.run.app")
+      .then((res) => res.text())
+      .then((data) => console.log(data))
+      .catch((err) => console.error("Function call error:", err));
+  }, []);
+
   return <h1 className="bg-red-400">Hello world!</h1>;
 }
 
